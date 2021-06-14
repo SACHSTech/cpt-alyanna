6cfd23638249eef449a78522e9b41f878728440a
alyannasantos <Alyanna.Santos21@ycdsbk12.ca>
2021-06-13 23:53:20 -0400

    create classes

---
+++ src/nbastats/Stat.java
@@ -0,0 +1,37 @@
+package nbastats;
+
+public class Stat {
+
+    private int rank = 0;
+    private String player = "";
+    private String position = "";
+    private int age = 0;
+    private String team = "";
+    private int gamesPlayed = 0;
+    private int gamesStarted = 0;
+    private int minPlayed = 0;
+    private int fieldGoal = 0;
+    private int fieldGoalAttempt = 0;
+    private double fieldGoalPct = 0.0;
+    private int threePoint = 0;
+    private int threePointAttempt = 0;
+    private double threePointPct = 0.0;
+    private int twoPoint = 0;
+    private int twoPointAttempt = 0;
+    private double twoPointPct = 0.0;
+    private double effectiveFieldGoal = 0.0;
+    private int freeThrow = 0;
+    private int freeThrowAttempt = 0;
+    private double freeThrowPct = 0.0;
+    private int offensiveRebound = 0;
+    private int defensiveRebound = 0;
+    private int totalRebound = 0;
+    private int assists = 0;
+    private int steals = 0;
+    private int blocks = 0;
+    private int turnovers = 0;
+    private int personalFouls = 0;
+    private int points = 0;
+
+    
+}
